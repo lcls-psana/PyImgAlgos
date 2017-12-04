0e23340872c1f610101cc7082bf208f7ab9f6de5
msdubrovin <dubrovin@slac.stanford.edu>
2017-12-04 09:31:15 -0800

    add/update doc

--- src/PeakData.py
+++
@@ -1,8 +0,0 @@
-#
-# leave it for backward compatability
-# due to merging of PeakData and TDPeak to TDPeakRecord
-#------------------------------
-
-from pyimgalgos.TDPeakRecord import TDPeakRecord as PeakData
-
-#------------------------------
